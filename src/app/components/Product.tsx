eafbcc44035843cfa8d2494560d00e9335b5763b
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-11-03 05:13:50 -0300

    component product and ProductImage

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,24 @@
+import { ProductType } from '../types/ProductType'
+import ProductImage from './ProductImage';
+
+type ProductProps = {
+  product: ProductType;
+}
+
+const Product = ({ product }: ProductProps) => {
+  return (
+    <div className='flex flex-col shadow-lg h-96 bg-slate-800 text-gray-300 p-5'>
+      <div className='relative max-h-72 flex-1'>
+        <ProductImage product={product} fill />
+      </div>
+      <div className='flex justify-between font-bold my-3'>
+        <p className='w-40 truncate'>{product.title}</p>
+        <p className='text-md text-teal-300'>{product.price}</p>
+        
+        </div>
+      <button className='rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center'>Adicionar ao carrinho</button>
+    </div>
+  )
+}
+
+export default Product
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Product from './components/Product';
 import { ProductType } from './types/ProductType';
 
 async function getProducts() {
@@ -17,9 +18,7 @@ export default async function Home() {
     <div className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'>
         {products.map((product: ProductType) => (
-          <div key={product.id}>
-            {product.title}
-          </div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
